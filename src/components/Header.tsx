5c8c2e0162709e2fa5ba697092bfa1073ffbc880
PabloCruzval <p.cruzat.valenzuela@gmail.com>
2025-06-21 00:08:46 -0400

    feat: Update recommendations page and header component
    - Added new Radix UI components for improved UI consistency.
    - Enhanced the recommendations page with better layout and search functionality.
    - Updated header component descriptions for clarity and consistency.
    - Refactored code for better readability and maintainability.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,19 +18,14 @@ const components: { title: string; href: string; description: string }[] = [
   {
     title: "Catálogo de Cursos",
     href: "/catalog",
-    description: "Descubre los cursos disponibles en este semestre en la universidad.",
+    description:
+      "Descubre los cursos disponibles en este semestre en la universidad.",
   },
   {
     title: "Sobre las Áreas de Formación General",
     href: "/docs/primitives/hover-card",
     description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      "Conoce las áreas de formación general y cómo se relacionan con los cursos.",
   },
 ];
 
@@ -38,20 +33,20 @@ export default function Header() {
   return (
     <header className="space-between tablet:space-x-8 flex w-full items-center py-8 px-4">
       <a href="/">
-      <img
-        src="logos/UC_Logo_Big.svg"
-        alt="UC Logo"
-        width={200}
-        height={200}
-        className="tablet:hidden h-20 w-auto"
-      />
-      <img
-        src="logos/UC_Logo_Small.svg"
-        alt="UC Logo"
-        width={200}
-        height={200}
-        className="tablet:block hidden h-20 w-auto"
-      />
+        <img
+          src="logos/UC_Logo_Big.svg"
+          alt="UC Logo"
+          width={200}
+          height={200}
+          className="tablet:hidden h-20 w-auto"
+        />
+        <img
+          src="logos/UC_Logo_Small.svg"
+          alt="UC Logo"
+          width={200}
+          height={200}
+          className="tablet:block hidden h-20 w-auto"
+        />
       </a>
 
       <nav className="tablet:block hidden w-full space-y-3">
@@ -85,31 +80,31 @@ export default function Header() {
                     <li className="row-span-3">
                       <NavigationMenuLink asChild>
                         <a
-                          className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none focus:shadow-md"
-                          href="/"
+                          className="from-foreground to-primary from-80% text-background hover:text-background hover:to-primary-foreground flex h-full w-full flex-col justify-end rounded-md bg-radial p-6 no-underline outline-none select-none focus:shadow-md"
+                          href="https://osuc.dev"
                         >
                           <div className="mt-4 mb-2 text-lg font-medium">
                             Creado por <br /> Open Source UC
                           </div>
-                          <p className="text-muted-foreground text-sm leading-tight">
-                            Beautifully designed components built with Radix UI
-                            and Tailwind CSS.
+                          <p className="text-background text-sm leading-tight">
+                            La comunidad estudiantil de innovación y desarrollo
+                            de software de la UC.
                           </p>
                         </a>
                       </NavigationMenuLink>
                     </li>
-                    <ListItem href="/docs" title="Introduction">
-                      Re-usable components built using Radix UI and Tailwind
-                      CSS.
-                    </ListItem>
-                    <ListItem href="/docs/installation" title="Installation">
-                      How to install dependencies and structure your apap.
+                    <ListItem href="/404" title="Cómo protegemos tu privacidad">
+                      Descubre cómo protegemos tu privacidad y datos personales.
                     </ListItem>
                     <ListItem
-                      href="/docs/primitives/typography"
-                      title="Typography"
+                      href="/404"
+                      title="RamosUC: Un espacio libre de acoso"
                     >
-                      Styles for headings, paragraphs, lists...etc
+                      Conóce cómo mantenemos un ambiente seguro y respetuoso.
+                    </ListItem>
+                    <ListItem href="/404" title="Conóce a nuestro equipo">
+                      Conóce a nuestro equipo de trabajo y cómo puedes
+                      colaborar.
                     </ListItem>
                   </ul>
                 </NavigationMenuContent>
@@ -121,13 +116,15 @@ export default function Header() {
             <ButtonNavbar text="Registrarse" url="/register" />
           </div>
         </div>
-      </div>
 
-      <div className="flex w-full space-x-2">
-        <ButtonNavbar text="BUSCACURSOS ORIGINAL" url="https://buscacursos.uc.cl/" />
-        <ButtonNavbar text="PORTAL UC" url="https://portal.uc.cl/" />
-        <ButtonNavbar text="CANVAS UC" url="https://cursos.canvas.uc.cl/" />
-      </div>
+        <div className="flex w-full space-x-2">
+          <ButtonNavbar
+            text="BUSCACURSOS ORIGINAL"
+            url="https://buscacursos.uc.cl/"
+          />
+          <ButtonNavbar text="PORTAL UC" url="https://portal.uc.cl/" />
+          <ButtonNavbar text="CANVAS UC" url="https://cursos.canvas.uc.cl/" />
+        </div>
       </nav>
     </header>
   );
